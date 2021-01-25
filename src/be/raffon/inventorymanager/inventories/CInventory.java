458ad0b1787c0c42695d41fe6028a31c543e4dec
Raffon-jar <raffoncervelle@gmail.com>
2021-01-25 15:48:40 +0100

    Implementing Cinventory

--- src/be/raffon/inventorymanager/inventories/CInventory.java
+++ src/be/raffon/inventorymanager/inventories/CInventory.java
@@ -1,7 +1,13 @@
 package be.raffon.inventorymanager.inventories;
 
+import java.util.ArrayList;
+
+import be.raffon.inventorymanager.listener.Events;
+
 public class CInventory {
 	
 	
-
+	public CInventory(ArrayList<Page> pages, Events events) {
+		
+	}
 }
--- src/be/raffon/inventorymanager/inventories/InventoryManager.java
+++ src/be/raffon/inventorymanager/inventories/InventoryManager.java
@@ -1,5 +1,11 @@
 package be.raffon.inventorymanager.inventories;
 
+import org.bukkit.inventory.Inventory;
+
 public class InventoryManager {
+
+	public Inventory getInventory() {
+		return null;
+	}
 
 }
